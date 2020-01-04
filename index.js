db67584cce8101dcc6e04aac43cce699444294fd
Pranav Sinha <pranav.sinha17@gmail.com>
2020-01-04 23:50:02 +0530

    Node Second Example on Express Extended one

--- index.js
+++ index.js
@@ -24,10 +24,33 @@ app.put("/dishes",(req,res,next)=>{
     res.statusCode=403;
     res.end("Put operations are not supported");
 });
+
 app.delete("/dishes",(req,res,next)=>{
     res.statusCode=200;
     res.end("Will delete all the dishes");
 });
+app.all("/dishes/:dishId",(req,res,next)=>{
+    res.statusCode=200;
+    next();
+    });
+    app.get("/dishes/:dishId",(req,res,next)=>{
+        res.statusCode=200;
+        res.end("Will send the dish detail of dish "+ req.params.dishId);
+    });
+    app.post("/dishes/:dishId",(req,res,next)=>{
+     res.statusCode=403;
+     res.end("Post Operations are not correct");
+    });
+    app.put("/dishes/:dishId",(req,res,next)=>{
+          res.statusCode=200;
+          res.end("Updated query will be on "+ req.params.dishId+
+          "\n The dish will be"+req.body.name+"and details "+req.body.description);
+    }
+          );
+    app.delete("/dishes/:dishId",(req,res,next)=>{
+    res.statusCode=200;
+    res.end("Deletion operation perform on "+ req.params.dishId);
+    });
 app.use((req,res,next)=>{
     console.log(req.header);
     res.statusCode=200;
