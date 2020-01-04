587c7e9a9f391a39e074b87a276fff42075919b6
Pranav Sinha <pranav.sinha17@gmail.com>
2020-01-04 23:27:13 +0530

    Node Express Second Example

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express=require('express');
 const http=require('http');
 const morgan=require('morgan');
+const bodyPasrer=require('body-parser');
+
 const app=express();
+app.use(bodyPasrer.json());
 app.use(morgan("dev"));
 app.use(express.static(__dirname+"/public"));
+
+app.all("/dishes",(req,res,next)=>{
+    res.statusCode=200;
+    next();
+});
+app.get("/dishes",(req,res,next)=>{
+    res.statusCode=200;
+    res.end("Will send you all the dishes ");
+});
+app.post("/dishes",(req,res,next)=>{
+    res.statusCode=200;
+    res.end("Will add the dish with"+req.body.name+" and detail"+req.body.description);
+});
+app.put("/dishes",(req,res,next)=>{
+    res.statusCode=403;
+    res.end("Put operations are not supported");
+});
+app.delete("/dishes",(req,res,next)=>{
+    res.statusCode=200;
+    res.end("Will delete all the dishes");
+});
 app.use((req,res,next)=>{
     console.log(req.header);
     res.statusCode=200;
